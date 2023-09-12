79dbc64fb4aab499266f251ac69905f5da2a2685
Gustavo Fiuza <55654983+GusFiuza@users.noreply.github.com>
2023-09-11 21:12:58 -0300

    new class

---
+++ src/main/java/br/com/fiuzaexplica/service/user.java
@@ -0,0 +1,11 @@
+package br.com.fiuzaexplica.service
+
+public class User() {
+    int id;
+    String name;
+
+    public String idAndName() {
+        return "My id is " + id + " and my name is " + name;
+    }
+
+}
