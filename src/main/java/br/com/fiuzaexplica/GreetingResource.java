16993d71b8fe89dd028fbfb3770b5336069a2c4c
Gustavo Fiuza <55654983+GusFiuza@users.noreply.github.com>
2023-09-16 22:11:20 +0000

    PathParam use

--- src/main/java/br/com/fiuzaexplica/GreetingResource.java
+++ src/main/java/br/com/fiuzaexplica/GreetingResource.java
@@ -3,6 +3,7 @@ package br.com.fiuzaexplica;
 import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -13,6 +14,25 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello(@DefaultValue("Fiuza") @QueryParam("name") String name) {
-        return "Hello " + name + "! Wellcome to the Java world!";
+        if (name.equals("Fiuza")) {
+            return "Hello " + name + "! Wellcome to the Java world!";
+        } else if (name.equals("Gustavo")) {
+            return "Hello " + name + "!";
+        } else {
+            return "Olá " + name + "!";
+        }
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{language}")
+    public String helloTranslation(@DefaultValue("Fiuza") @QueryParam("name") String name, @PathParam("language") String language) {
+        if (language.equals("English")) {
+            return "Hello " + name + "! Wellcome to the Java world!";
+        } else if (language.equals("Portuguese")) {
+            return "Olá " + name + "! Bem vindo ao mundo Java!";
+        } else {
+            return "Hello " + name + "! I don't speak your language.";
+        }
     }
 }
